8d4ab5068ed4b324327826b3af0ec2769d176e30
ellamusen <dani_1997@live.dk>
2020-09-17 14:34:39 +0200

    A program that prompts the user to enter the starting velocity v0 in meters/second, the ending velocity v1 in meters/second, and the time span t in seconds, then displays the average acceleration.

--- src/Chapter_2/Exercise_2_9_physics_acceleration.java
+++ src/Chapter_2/Exercise_2_9_physics_acceleration.java
@@ -1,10 +1,26 @@
 package Chapter_2;
 
+import java.text.*;
+import java.util.Scanner;
+
 public class Exercise_2_9_physics_acceleration {
 
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Physics: Acceleration");
+        System.out.print("Enter v0, v1 and t: ");
+
+        double v0 = input.nextDouble();
+        double v1 = input.nextDouble();
+        double t = input.nextDouble();
+
+        double a = (v1 - v0) / t;
+
+        DecimalFormat df = new DecimalFormat("#.##");
+
+        System.out.println("The average acceleration is " + df.format(a) );
 
     }
 
