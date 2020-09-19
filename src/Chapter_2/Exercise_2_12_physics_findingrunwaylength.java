814d33d6c853ee32f4fbb247e59963debdc57429
ellamusen <dani_1997@live.dk>
2020-09-19 12:57:24 +0200

    Finished assignment

--- src/Chapter_2/Exercise_2_12_physics_findingrunwaylength.java
+++ src/Chapter_2/Exercise_2_12_physics_findingrunwaylength.java
@@ -1,10 +1,25 @@
 package Chapter_2;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Exercise_2_12_physics_findingrunwaylength {
 
-    // Members / values to class
+    public static void main(String[] args) {
 
+        System.out.println("Psychics: Finding runway length");
 
-    // Methods
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter speed and acceleration: "); // Prompt the user to enter v in meters/seconds and the acceleration a in meters/second squared
+        double v = input.nextDouble(); // Take-off speed
+        double a2 = input.nextDouble(); // Acceleration
+
+        double length = (v * v) / (a2 * 2); // Formula
+
+        DecimalFormat df = new DecimalFormat("#.###");
+
+        System.out.println("The minimum runway length for this airplane is  " + df.format(length));
+
+    }
 }
