6b761c92b3c32651679071ad87b6ed778ca06673
ellamusen <dani_1997@live.dk>
2020-09-19 11:30:43 +0200

    Finished assignment

--- src/Chapter_2/Exercise_2_10_science_calculatingenergy.java
+++ src/Chapter_2/Exercise_2_10_science_calculatingenergy.java
@@ -1,10 +1,29 @@
 package Chapter_2;
 
+import java.util.Scanner;
+
 public class Exercise_2_10_science_calculatingenergy {
 
-    // Members / values to class
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Science: Calculating energy");
+
+        System.out.print("Enter the amount of water in kilograms: "); // Prompt the user to enter value
+        double m = input.nextDouble(); // The weight of water in kilograms
+
+        System.out.print("Enter the initial temperature of the water: "); // Prompt the user to enter value
+        double initialTemperature = input.nextDouble(); // Temperature measured in Celsius
+
+        System.out.print("Enter the final temperatures of the water: "); // Prompt the user to enter value
+        double finalTemperature = input.nextDouble(); // Temperature measured in Celsius
 
 
-    // Methods
+        double q = m * (finalTemperature - initialTemperature) * 4184; // Formula (Energy Q is measured in joules)
+        System.out.println("The energy needed is " + q);
+
+    }
 
 }
+
