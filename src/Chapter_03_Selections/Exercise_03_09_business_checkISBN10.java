11b79736a40e0e34b5c957d7f385b15314c985ff
ellamusen <dani_1997@live.dk>
2020-11-06 11:19:35 +0100

    Replaced d1 * 1 with d1

--- src/Chapter_03_Selections/Exercise_03_09_business_checkISBN10.java
+++ src/Chapter_03_Selections/Exercise_03_09_business_checkISBN10.java
@@ -21,7 +21,7 @@ public class Exercise_03_09_business_checkISBN10 {
         int d8 = input.nextInt();
         int d9 = input.nextInt();
 
-        int d10 = ((d1 * 1) + (d2 * 2) + (d3 * 3) + (d4 * 4) + (d5 * 5) + (d6 * 6) + (d7 * 7) + (d8 * 8) + (d9 * 9)) % 11;
+        int d10 = ((d1) + (d2 * 2) + (d3 * 3) + (d4 * 4) + (d5 * 5) + (d6 * 6) + (d7 * 7) + (d8 * 8) + (d9 * 9)) % 11;
 
         if (d10 == 10) {
             System.out.println("The ISBN-10 number is " + d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + "X");
