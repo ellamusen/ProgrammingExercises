848e42931997015caa24b95a0f94ebfcd3d995d4
ellamusen <dani_1997@live.dk>
2020-11-06 11:18:50 +0100

    Finished exercise

---
+++ src/Chapter_03_Selections/Exercise_03_09_business_checkISBN10.java
@@ -0,0 +1,33 @@
+package Chapter_03_Selections;
+
+import java.util.Scanner;
+
+public class Exercise_03_09_business_checkISBN10 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Business: check ISBN-10");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter the first 9 digits of an ISBN as integers: ");
+        int d1 = input.nextInt();
+        int d2 = input.nextInt();
+        int d3 = input.nextInt();
+        int d4 = input.nextInt();
+        int d5 = input.nextInt();
+        int d6 = input.nextInt();
+        int d7 = input.nextInt();
+        int d8 = input.nextInt();
+        int d9 = input.nextInt();
+
+        int d10 = ((d1 * 1) + (d2 * 2) + (d3 * 3) + (d4 * 4) + (d5 * 5) + (d6 * 6) + (d7 * 7) + (d8 * 8) + (d9 * 9)) % 11;
+
+        if (d10 == 10) {
+            System.out.println("The ISBN-10 number is " + d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + "X");
+        }
+        else {
+            System.out.println("The ISBN-10 number is " + d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10);
+        }
+    }
+}
